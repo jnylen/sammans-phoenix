599e889be0c391178e089bd9794f869075936f3f
Joakim Nylén <git@joakim.nylen.nu>
2020-06-30 01:16:02 +0200

    Fix?

--- config/releases.exs
+++ config/releases.exs
@@ -35,7 +35,7 @@ config :sammans, SammansWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :sammans, SammansWeb.Endpoint, server: true
+config :sammans, SammansWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
